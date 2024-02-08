3d9f173abdd2f58d5df6040f53cd2e24a440d50f
Kaviya P <129056758+KAVIYAP30@users.noreply.github.com>
2024-02-08 17:21:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,10 @@ import { useState,useEffect } from 'react'
 function App() {
   const initialState={
    transactions: [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
+        // { id: 1, text: 'Flower', amount: -20 },
+        // { id: 2, text: 'Salary', amount: 300 },
+        // { id: 3, text: 'Book', amount: -10 },
+        // { id: 4, text: 'Camera', amount: 150 }
       ],
   }
 const [transactions,setTransactions]=useState(initialState.transactions);
